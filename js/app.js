810dc147e10a75dfc4fd05c571c40e97eb8c426e
enzoozaki <enzojapa112gmail.com>
2025-10-04 09:03:49 -0300

    retorno do botão de confirmação

--- js/app.js
+++ js/app.js
@@ -1,34 +1,37 @@
-function alterarStatus (id) {
-    
-    let item = document.getElementById(`game-${id}`);
+let jogosAlugados = 0;
 
 
-    let botao = item.querySelector('.dashboard__item__button');
-
-    let imgConteiner = item.querySelector('.dashboard__item__img');
-
-
-    if(botao.textContent === 'Alugar') {
-       
-        botao.textContent = 'Devolver';
-
-
-        botao.style.backgroundColor = 'black';
-        botao.style.color = 'white';
-
-
-        imgConteiner.classList.add('dashboard__item__img--rented');
-    
-    } else {
-       
-      
-        botao.textContent = 'Alugar';
-
-
-        botao.style.backgroundColor = '#505EFF';
-        botao.style.color = 'black';
-
-
-        imgConteiner.classList.remove('dashboard__item__img--rented');
-    }
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
 }
+
+
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        jogosAlugados--;
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+        jogosAlugados++;
+    }
+
+
+     return contarEExibirJogosAlugados();
+}
+
+
+// Inicializa a contagem considerando que os jogos já começam alugados
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
